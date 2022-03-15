3532e7086a951f09306c6a0a865ea559fcf2427c
Luiz Felipe dos Santos <luizfelipesantos.sc@gmail.com>
2022-03-15 14:26:52 +0100

    Iteration 6 Done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,23 @@
 const router = require("express").Router();
+const Movie = require("../models/Movies.model");
+const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
+
+router.get('/movies/create', async (req, res) => {
+    const celebrities = await Celebrity.find();
+    res.render('movies/new-movie.hbs', { celebrities }); 
+});
+
+router.post('/movies/create', async (req, res) => {
+    try {
+        const newMovie = new Movie({ ...req.body });
+        await newMovie.save();
+        res.redirect("/movies");
+      } catch (err) {
+        console.error(err);
+        res.redirect("/movies/create");
+      }
+});
 
 module.exports = router;
