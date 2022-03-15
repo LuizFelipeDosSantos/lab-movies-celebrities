5cb9e8fb234002631f59ccfa9f2e454c2b8a7e68
Renato Costa <renatoaccosta@gmail.com>
2022-03-15 11:32:50 +0000

    Iteration 3 Done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,18 @@
+const async = require("hbs/lib/async");
 const router = require("express").Router();
-
+const Celebrity = require("../models/Celebrity.model");
 // all your routes here
-
+router.get("/celebrities/create", async (req, res) => {
+  res.render("celebrities/new-celebrity.hbs");
+});
+router.post("/celebrities/create", async (req, res) => {
+  try {
+    const newCelebrity = new Celebrity({ ...req.body });
+    await newCelebrity.save();
+    res.redirect("/celebrities");
+  } catch (err) {
+    console.error(err);
+    res.redirect("/celebrities/create");
+  }
+});
 module.exports = router;
