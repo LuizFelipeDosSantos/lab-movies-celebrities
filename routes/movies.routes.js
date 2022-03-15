1e9fd547e50515f10a0ca333946134429da3c7f6
Luiz Felipe dos Santos <luizfelipesantos.sc@gmail.com>
2022-03-15 14:50:43 +0100

    Iteration 8 Done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Movie = require("../models/Movies.model");
 const Celebrity = require("../models/Celebrity.model");
+const mongoose = require("mongoose");
 
 // all your routes here
 router.get("/movies", async (req, res) => {
@@ -21,6 +22,17 @@ router.post("/movies/create", async (req, res) => {
   } catch (err) {
     console.error(err);
     res.redirect("/movies/create");
+  }
+});
+
+router.get("/movies/:id", async (req, res) => {
+  try {
+    const movieId = mongoose.Types.ObjectId(req.params.id);
+    const movie = await Movie.findById(movieId);
+    await movie.populate('cast');
+    res.render("movies/movie-details.hbs", {movie});
+  } catch (err) {
+    console.error(err);
   }
 });
 
