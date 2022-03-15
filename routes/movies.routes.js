35ab982b7684ce817822d099b1af3ce0af4a61f7
Renato Costa <renatoaccosta@gmail.com>
2022-03-15 13:32:24 +0000

    Iteration 7 done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -3,21 +3,25 @@ const Movie = require("../models/Movies.model");
 const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
-
-router.get('/movies/create', async (req, res) => {
-    const celebrities = await Celebrity.find();
-    res.render('movies/new-movie.hbs', { celebrities }); 
+router.get("/movies", async (req, res) => {
+  const allmovies = await Movie.find();
+  res.render("movies/movies.hbs", { allmovies });
 });
 
-router.post('/movies/create', async (req, res) => {
-    try {
-        const newMovie = new Movie({ ...req.body });
-        await newMovie.save();
-        res.redirect("/movies");
-      } catch (err) {
-        console.error(err);
-        res.redirect("/movies/create");
-      }
+router.get("/movies/create", async (req, res) => {
+  const celebrities = await Celebrity.find();
+  res.render("movies/new-movie.hbs", { celebrities });
+});
+
+router.post("/movies/create", async (req, res) => {
+  try {
+    const newMovie = new Movie({ ...req.body });
+    await newMovie.save();
+    res.redirect("/movies");
+  } catch (err) {
+    console.error(err);
+    res.redirect("/movies/create");
+  }
 });
 
 module.exports = router;
